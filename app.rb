1723667242b238727de4b734d40d5c7ddbcf52ff
zachary mines <zmines@wesleyan.edu>
2020-09-15 23:27:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
-
+configure do 
+  enable :sessions 
+  set :session_secret, "habbaiojfweoijfewoij"
+end 
 class App < Sinatra::Base
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    
+  end 
 end
